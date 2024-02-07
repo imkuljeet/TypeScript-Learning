2ecc293ca1b301beb66f145c70777a137d4d4a40
imkuljeet <iamkkashyap@gmail.com>
2024-02-07 20:48:21 +0530

    Working with Union Types

--- app.ts
+++ app.ts
@@ -3,8 +3,13 @@ const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
 
-function add(num1: any,num2: number){
-    return num1 + num2;
+function add(num1: number | string,num2: number | string){
+    if(typeof num1 === 'number' && typeof num2 === 'number'){
+        return num1 + num2;
+    }else if(typeof num1 ==='string' && typeof num2 ==='string'){
+        return num1+" "+num2;
+    }
+    return +num1 + +num2;
 }
 
 console.log(add(1,6));
@@ -15,6 +20,10 @@ buttonElement.addEventListener('click',()=>{
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1,+num2);
+    const stringResult = add(num1, num2);
+
 
     console.log(result);
+    console.log(stringResult);
+    // console.log(add(true,false));
 })
