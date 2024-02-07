5e798a32a973b878ec36de50b9a65d32a224c079
imkuljeet <iamkkashyap@gmail.com>
2024-02-07 20:29:59 +0530

    Configuring TypeScript

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-var num1Element = document.getElementById('num1');
-var num2Element = document.getElementById('num2');
-var buttonElement = document.querySelector('button');
+"use strict";
+const num1Element = document.getElementById('num1');
+const num2Element = document.getElementById('num2');
+const buttonElement = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
 console.log(add(1, 6));
 // console.log(add('1','6'));
-buttonElement.addEventListener('click', function () {
-    var num1 = num1Element.value;
-    var num2 = num2Element.value;
-    var result = add(+num1, +num2);
+buttonElement.addEventListener('click', () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const result = add(+num1, +num2);
     console.log(result);
 });
